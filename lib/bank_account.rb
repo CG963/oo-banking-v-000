9029eab165c0db087f8dc18ff2dee0e1450a7681
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-05 07:45:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,21 +7,21 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
-  
+
   def deposit(amount)
     self.balance += amount
   end
-  
+
   def display_balance
     "Your balance is $#{balance}."
   end
-  
+
   def close_account
     self.status = "closed"
   end
-  
+
   def valid?
     balance > 0 && status == "open"
   end
-  
+
 end
