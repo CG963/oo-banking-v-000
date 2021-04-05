e72c7211cbfc9414e0dc8ac8c34820534eeb043d
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-05 07:53:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,6 +5,9 @@ class Transfer
     @sender = sender
     @receiver = receiver
     @amount = amount
+  end
+  
+  def valid?
   
 end
 
@@ -16,27 +19,7 @@ describe 'Transfer' do
   let(:transfer) { Transfer.new(amanda, avi, 50) }
   let(:bad_transfer) { Transfer.new(amanda, avi, 4000) }
 
-  describe 'initialize' do
-    it "can initialize a Transfer" do
-      expect(transfer).to be_a(Transfer)
-    end
-
-    it "initializes with a sender" do
-      expect(transfer.sender).to eq(amanda)
-    end
-
-    it "initializes with a receiver" do
-      expect(transfer.receiver).to eq(avi)
-    end
-
-    it "always initializes with a status of 'pending'" do
-      expect(transfer.status).to eq("pending")
-    end
-
-    it "initializes with a transfer amount" do
-      expect(transfer.amount).to eq(50)
-    end
-  end
+ 
 
   describe '#valid?' do
     it "can check that both accounts are valid" do
